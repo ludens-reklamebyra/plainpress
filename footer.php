1348a31edfc55b481837d8aad2377946f55c219a
Stian Bakkane <stianba@gmail.com>
2015-12-02 14:28:59 +0100

    Moved app.js from functions.hp to bottom of body in footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+        <script
+            src="<?php bloginfo('template_url'); ?>/assets/js/app.js"
+            charset="utf-8">
+        </script>
         <?php wp_footer (); ?>
     </body>
 </html>
--- functions.php
+++ functions.php
@@ -2,8 +2,6 @@
 function theme_name_scripts() {
 	wp_enqueue_style("style-name",
         get_template_directory_uri()."/assets/css/app.css");
-	wp_enqueue_script("script-name",
-        get_template_directory_uri() . "/assets/js/app.js");
 }
 
 add_action("wp_enqueue_scripts", "theme_name_scripts");

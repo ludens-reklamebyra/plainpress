8ce0106b2cbdd4d2fa7a1d4f1886d712c7263526
oyvindhermansen <oyvind@ludensreklame.no>
2017-04-26 22:48:19 +0200

    fixed #21 , added polyfill.io

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
         <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+        <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
         <?php wp_head(); ?>
     </head>
     <body>

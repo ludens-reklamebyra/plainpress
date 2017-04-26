85f3beb8ce45e9f7c18f622b504ec0de5e27c03e
oyvindhermansen <oyvind@ludensreklame.no>
2017-04-26 23:27:45 +0200

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,3 @@ import $ from 'jquery';
 $(document).ready(() => {
   // Write bugfree code
 });
-
-const someFunc = ({ hello, world, lol, damn, perf, satan, loled, bug, prettier, prett  }) => {
-  return {};
-};

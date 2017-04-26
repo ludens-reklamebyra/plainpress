91589315c13086f49d29584ac2defddde65ad706
oyvindhermansen <oyvind@ludensreklame.no>
2017-04-26 23:12:45 +0200

    last test

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,6 @@ $(document).ready(() => {
   // Write bugfree code
 });
 
-const someFunc = ({ hello, world, lol, damn, perf, satan, loled, bug, prettier  }) => {
+const someFunc = ({ hello, world, lol, damn, perf, satan, loled, bug, prettier, haha  }) => {
   return {};
 };

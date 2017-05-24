ad199bab57e9764c6abfb1cf847d5b1efbf39ad5
oyvindhermansen <oyvind@ludensreklame.no>
2017-05-24 23:20:28 +0200

    prettier

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,4 +3,12 @@ import $ from 'jquery';
 $(document).ready(() => {
   // Write bugfree code
   console.log('App is running');
+
+  const thisIsAFunctionJustToTestPrettierInAction = (someArg, someArg2, someArg3) => {
+    return {
+      someArg,
+      someArg2,
+      someArg3
+    };
+  };
 });
